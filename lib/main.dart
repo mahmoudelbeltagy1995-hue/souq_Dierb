bf5180199b47c215f0b217759d26dc72806f09d5
Mahmoud Hamdy <mahmouashwah@gmail.com>
2024-03-15 01:28:57 +0200

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,17 @@
-<<<<<<< HEAD
-// ignore_for_file: unused_local_variable
-
-import 'package:firebase_core/firebase_core.dart';
-=======
->>>>>>> a7689cc
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:t_store/core/cubits/banner_carousel_slider_cubit_cubit/banner_carousel_slider_cubit.dart';
 import 'package:t_store/core/cubits/navigation_menu_cubit/navigation_menu_cubit.dart';
 import 'package:t_store/core/utils/constants/text_strings.dart';
 import 'package:t_store/core/utils/theme/theme.dart';
-import 'package:t_store/features/auth/data/repositories/authentication_repository.dart';
 import 'package:t_store/features/auth/presentation/cubit/on_boarding_cubit.dart';
 import 'package:t_store/features/auth/presentation/views/on_boarding/on_boarding_view.dart';
 
 void main() async {
-<<<<<<< HEAD
-  // widgets binding
-  final WidgetsBinding widgetsBinding =
-      WidgetsFlutterBinding.ensureInitialized();
-// local Storage
-  await GetStorage.init();
-// splash screen
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-//firebase initialization
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  ).then((value) => Get.put(AuthenticationRepository()));
-=======
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-//firebase initialization
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
 
-  // setupServiceLocator();
-
->>>>>>> a7689cc
   runApp(const MyApp());
 }
 
@@ -48,6 +19,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
     return MultiBlocProvider(
       providers: [
         BlocProvider(
@@ -60,7 +32,7 @@ class MyApp extends StatelessWidget {
           create: (context) => BannerCarouselSliderCubit(),
         )
       ],
-      child: GetMaterialApp(
+      child: MaterialApp(
         title: TTexts.appName,
         themeMode: ThemeMode.system,
         theme: TAppTheme.lightTheme,
