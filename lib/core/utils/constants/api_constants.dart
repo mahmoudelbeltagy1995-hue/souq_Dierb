bbccd830816be6e88c1716bdceca28c3511697cb
mahmoodhamdi <hmdy7486@gmail.com>
2024-07-30 22:50:17 +0300

    Add API constants for product, category, and user endpoints

--- lib/core/utils/constants/api_constants.dart
+++ lib/core/utils/constants/api_constants.dart
@@ -1,5 +1,11 @@
-
-/* -- LIST OF Constants used in APIs -- */
-
-// Example
-const String tSecretAPIKey = "cwt_live_b2da6ds3df3e785v8ddc59198f7615ba";
+class ApiConstants {
+  static const String baseUrl = 'https://api.escuelajs.co/api/v1/';
+  static String getProducts() => '${baseUrl}products';
+  static String getProductById(String id) => '${baseUrl}products/$id';
+  static String getCategories() => '${baseUrl}categories';
+  static String getCategoryById(String id) => '${baseUrl}categories/$id';
+  static String getUsers() => '${baseUrl}users';
+  static String getUserById(String id) => '${baseUrl}users/$id';
+  static String login() => '${baseUrl}auth/login';
+  static String getProfile() => '${baseUrl}auth/profile';
+}
